cc3fe0f62c144fbead23e588215ffc8e05df1caa
Blake Rose <bdecktor@gmail.com>
2019-05-05 19:06:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def now_serving (katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   end
-  katz.deli.pop
+  puts katz.deli.pop
 #  puts katz_deli[0]
 #   end
 #   katz_deli.shift
